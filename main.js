1106603f0f1a6c3846f2190a2fa29cc91d7664b6
Carlos Rodriguez <cfrodriguezb17@gmail.com>
2022-08-05 10:42:54 -0500

    Resize grid working, project finished

--- main.js
+++ main.js
@@ -2,14 +2,17 @@ let container = document.querySelector('#container');
 let buttonStart = document.querySelector('#buttonStart'); 
 let working = false;
 let work = () => working ? working = false : working = true;
-let addBox = () => {
+let addBox = (w) => {
+  let breadth = 960 / w;
+  console.log(breadth);
   const box = document.createElement('div');
+  box.style.cssText = `width: ${breadth}px; height: ${breadth}px;`;
   box.classList.add('box');
   container.appendChild(box);
 }
 let printMapLine = (x) => {
   for(i = 0; i < x; i++){
-    addBox();
+    addBox(x);
   }
 }
 let printMap = (x = 16, y = 16) => {
@@ -21,6 +24,8 @@ let selectGrid = () => {
   container.innerHTML = '';
   let x = Number(prompt('How many rows do you want?'));
   let y = Number(prompt('How many columns do you want?'));
+  if (y > 100) y = 100
+  if (x > 100) x = 100
   container.style.cssText = `grid-template-columns: repeat(${y}, 1fr);
   grid-template-rows: repeat(${x}, 1fr);`;
   printMap(x, y);
@@ -46,6 +51,12 @@ let smartNow = () => {
   }
   if(working == true){
     drawing();
+  }
+}
+let resetMap = () => {
+  let divs = container.querySelectorAll('div');
+  for (let div of divs){
+    div.style.background = 'grey';
   }
 }
 printMap();
